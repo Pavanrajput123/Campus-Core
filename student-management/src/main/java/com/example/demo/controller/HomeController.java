7256780f7ee4387145145c53350a29165e3c1650
Pavan Rajput <pavanrajput1701@gmail.com>
2025-04-14 21:58:44 +0530

    Initial commit - Campus Core project upload

---
+++ student-management/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "homepage"; // This corresponds to homepage.html in templates folder
+    }
+}
